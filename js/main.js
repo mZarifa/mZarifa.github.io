48cb8e7c75892dda0e24abcc5ff6eca635e144de
mZarifa <matthewzarifa@gmail.com>
2021-05-06 20:55:08 -0600

    Added animations, added slideshow gallery

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
 	'use strict';
 
 
-
 	var isMobile = {
 		Android: function() {
 			return navigator.userAgent.match(/Android/i);
@@ -304,6 +303,5 @@
 		stickyFunction();
 		owlCrouselFeatureSlide();
 	});
-
 
 }());
